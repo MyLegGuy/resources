825a6c9e4405bedcedbd39c85514060d15cdb878
EliEron <subanimehd@gmail.com>
2017-07-26 16:44:47 +0200

    Change extraction file order
    Extract "Tatarigoroshi.Voice.and.Graphics.Patch.v3.0.0.zip" after "Tatarigoroshi-CG.zip".
    
    This fixes 07th-mod/tatarigoroshi/issues/33

--- tatarigoroshi/install.bat
+++ tatarigoroshi/install.bat
@@ -35,8 +35,8 @@ echo.
 timeout /t 1 > nul
 .\7za.exe x Tatarigoroshi-CGAlt.zip
 .\7za.exe x Tatarigoroshi-Voices.zip
-echo A | .\7za.exe x Tatarigoroshi.Voice.and.Graphics.Patch.v3.0.0.zip
 .\7za.exe x Tatarigoroshi-CG.zip
+echo A | .\7za.exe x Tatarigoroshi.Voice.and.Graphics.Patch.v3.0.0.zip
 rmdir /S /Q ..\CG > nul
 rmdir /S /Q ..\CGAlt > nul
 timeout /t 1 > nul
